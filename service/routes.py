5db41ccccab58cb06ac3443d62179021a8d1a22a
Thobac-ctrl <Thobac23@gmail.com>
2023-08-14 06:36:11 -0400

    change routes.py according to flake8 report

--- service/routes.py
+++ service/routes.py
@@ -122,6 +122,7 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -151,3 +152,4 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
+    
